195decbc87c04cc941b668f1279319f6eaa74a0b
eyal-david <eyal0228@gmail.com>
2023-01-03 13:46:21 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { ProductCard } from "components";
 import { Product } from "shared/models";
 
 function App() {
   const [MockRes, setData] = useState<Product[]>([]);
-  setData([]);
+  useEffect(() => {
+    (async () => {
+      const res: Product[] = await fetch("/products/all").then((res) =>
+        res.json()
+      );
+      setData(res);
+    })();
+  }, []);
+
   return (
     <div className="App">
       {MockRes.map((item) => (
--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,12 @@ import "./index.css";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+if (process.env.NODE_ENV === "development") {
+  const { worker } = require("./mocks");
+  worker.start();
+}
+
 root.render(
   <React.StrictMode>
     <App />
